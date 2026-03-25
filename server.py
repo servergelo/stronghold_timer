898d2ef767be9c2a2c2f84b722933edfe87fc990
Angelo <geloo.balagtas@gmail.com>
2026-03-25 22:29:04 +0800

    Add files via upload

--- server.py
+++ server.py
@@ -13,7 +13,6 @@ CORS(app, resources={r"/*": {"origins": "*"}})
 socketio = SocketIO(
     app,
     cors_allowed_origins="*",
-    async_mode='threading',
     ping_timeout=60,
     ping_interval=25,
     engineio_logger=False,
